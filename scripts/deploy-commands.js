1e820ea76273d9c6de0692b40452d2589729932a
Navi <mailvoornavi@gmail.com>
2022-04-30 15:36:42 +0200

    updated for application-wide commands

--- scripts/deploy-commands.js
+++ scripts/deploy-commands.js
@@ -1,7 +1,7 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('../config.json');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const { clientId, token } = require('../config.json')
 const { readdirSync } = require('fs')
 const ascii = require('ascii-table')
 const path = require('path')
@@ -10,19 +10,19 @@ const path = require('path')
 const folders = ["utility"]
 
 // Initializing the table
-const table = new ascii("Commands")
-table.setHeading("Commands", "Load Status")
+const table = new ascii("Interactions")
+table.setHeading("Interaction", "Load Status")
 
 // define the slash commands
 const slashCommands = []
 
 // Lets get all the subfolders of the "commands" folder
 folders.forEach(dir => {
-	const commands = readdirSync(path.join(__dirname, `../src/commands/${dir}/`)).filter(d => d.endsWith(".js"))
+	const commands = readdirSync(path.join(__dirname, `../src/interactions/${dir}/`)).filter(d => d.endsWith(".js"))
 
 	for (let file of commands) {
 		// Now, we will get every command from every subfolder
-		let pull = require(`../src/commands/${dir}/${file}`)
+		let pull = require(`../src/interactions/${dir}/${file}`)
 		// We check if the command has a configuration
 		if(pull.name) {
 			// If it does, add a row to the table and finally set the command
@@ -32,17 +32,17 @@ folders.forEach(dir => {
 			slashCommands.push(new SlashCommandBuilder().setName(pull.command).setDescription(pull.description))
 		} else {
 			// If the command doesn't have a valid configuration, add another row showing the error
-			table.addRow(file, `❌ --> Missing help.name or help.name is not a string`)
-			continue;
+			table.addRow(file, `❌ --> Missing ${file}.name or ${file}.name is not a string`)
+			continue
 		}
 	}
-
-    console.log(table.toString())
 })
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(token)
 
 // register the commands
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: slashCommands.map(command => command.toJSON()) })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest.put(Routes.applicationCommands(clientId), { body: slashCommands.map(command => command.toJSON()) })
+	.then(() => console.log('Successfully registered application interactions.'))
+	.catch(console.error)
+
+console.log(table.toString())
--- scripts/remove-commands.js
+++ scripts/remove-commands.js
@@ -1,17 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('../config.json');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const { clientId, token } = require('../config.json')
     
-const rest = new REST({ version: '9' }).setToken(token);
-rest.get(Routes.applicationGuildCommands(clientId, guildId))
+const rest = new REST({ version: '9' }).setToken(token)
+rest.get(Routes.applicationCommands(clientId))
     .then(data => {
-        const promises = [];
+        const promises = []
         for (const command of data) {
-            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
-            promises.push(rest.delete(deleteUrl));
+            const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`
+            promises.push(rest.delete(deleteUrl))
         }
-		console.log('Successfully de-registered application commands.')
-        return Promise.all(promises);
+		console.log('Successfully de-registered application interactions.')
+        return Promise.all(promises)
     })
-	.catch(console.error);
+	.catch(console.error)
