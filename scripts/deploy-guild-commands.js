9b8120ff2c8c9dfc8b965058dc5c0b5f2417f3b9
Navi <mailvoornavi@gmail.com>
2022-04-30 15:36:53 +0200

    added guild-level command deployment

---
+++ scripts/deploy-guild-commands.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const { clientId, guildId, token } = require('../config.json')
+const { readdirSync } = require('fs')
+const ascii = require('ascii-table')
+const path = require('path')
+
+// define the command folders
+const folders = ["utility"]
+
+// Initializing the table
+const table = new ascii("Interactions")
+table.setHeading("Interaction", "Load Status")
+
+// define the slash commands
+const slashCommands = []
+
+// Lets get all the subfolders of the "commands" folder
+folders.forEach(dir => {
+	const commands = readdirSync(path.join(__dirname, `../src/interactions/${dir}/`)).filter(d => d.endsWith(".js"))
+
+	for (let file of commands) {
+		// Now, we will get every command from every subfolder
+		let pull = require(`../src/interactions/${dir}/${file}`)
+		
+		// We check if the command has a configuration
+		if(pull.name) {
+			// If it does, add a row to the table and finally set the command
+			table.addRow(file, '✔️')
+			
+			// build the command
+			let command = new SlashCommandBuilder()
+
+			command.setName(pull.command)
+			command.setDescription(pull.description)
+
+			// todo: ad more options
+
+			// add command to table
+			slashCommands.push(command)
+		} else {
+			// If the command doesn't have a valid configuration, add another row showing the error
+			table.addRow(file, `❌ --> Missing ${file}.name or ${file}.name is not a string`)
+			continue
+		}
+	}
+})
+
+const rest = new REST({ version: '9' }).setToken(token)
+
+// register the commands
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: slashCommands.map(command => command.toJSON()) })
+	.then(() => console.log('Successfully registered guild interactions.'))
+	.catch(console.error)
+
+console.log(table.toString())
